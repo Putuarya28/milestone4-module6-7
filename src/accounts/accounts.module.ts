9ca4a1e0c96c195960d7578d0c683a5218829575
Putu Arya Wibawa <mail.aryawibawa@gmail.com>
2025-07-26 02:35:04 +0300

    minor error fixes

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { AccountsController } from "./accounts.controller";
+import { AccountsService } from "./accounts.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
   imports: [PrismaModule],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
-import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { UsersModule } from "../users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'defaultSecret',
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET || "defaultSecret",
+      signOptions: { expiresIn: "1h" },
     }),
+    PrismaModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
