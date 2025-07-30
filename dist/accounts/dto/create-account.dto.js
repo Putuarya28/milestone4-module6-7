a1e77e48a692eb1af1ffa56a839e93fed5ec5365
Putu Arya Wibawa <mail.aryawibawa@gmail.com>
2025-07-30 17:46:40 +0300

    remove dist

--- dist/accounts/dto/create-account.dto.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateAccountDto = void 0;
-class CreateAccountDto {}
-exports.CreateAccountDto = CreateAccountDto;
