2584082f006ae7a3a6c0c49c8d5c85e28ab62f35
Putu Arya Wibawa <mail.aryawibawa@gmail.com>
2025-07-31 08:38:52 +0300

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  // Swagger setup
-  const config = new DocumentBuilder()
-    .setTitle("RevoBank API")
-    .setDescription("API documentation for RevoBank")
-    .setVersion("1.0")
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
-
-  await app.listen(process.env.PORT || 3000, "0.0.0.0");
+  await app.listen(3000);
 }
 bootstrap();
