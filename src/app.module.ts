96ac2fa517ede2d22cfadb112466fb70e7a4dbc3
Putu Arya Wibawa <mail.aryawibawa@gmail.com>
2025-07-31 05:52:31 +0300

    adding health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AccountsModule } from './accounts/accounts.module';
-import { TransactionsModule } from './transactions/transactions.module';
-import { PrismaService } from './prisma/prisma.service';
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { AccountsModule } from "./accounts/accounts.module";
+import { TransactionsModule } from "./transactions/transactions.module";
+import { PrismaService } from "./prisma/prisma.service";
+import { HealthController } from "./health.controller";
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    AccountsModule,
-    TransactionsModule,
-  ],
+  imports: [AuthModule, UsersModule, AccountsModule, TransactionsModule],
+  controllers: [HealthController],
   providers: [PrismaService],
 })
 export class AppModule {}
